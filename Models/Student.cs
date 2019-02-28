b5e500e6c26529f673f716362312ce1ecd718ef9
saranyats <43873219+saranyats@users.noreply.github.com>
2019-02-28 16:34:04 +0530

    Add files via upload
    ASP.NET-MVC:28/02/2019

---
+++ Models/Student.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+namespace Myproject.Models
+{
+    public class Student
+    {
+        [Required(ErrorMessage="cant be empty")]
+        [Display(Name="Student name")]
+        //[RegularExpression()]
+        public string Sname { get; set; }
+        [Required(ErrorMessage = "cant be empty")]
+        [Display(Name = "Student Adress")]
+        [DataType(DataType.MultilineText)]
+        public string Saddress { get; set; }
+        [Required(ErrorMessage = "cant be empty")]
+        [Display(Name = "email")]
+        [DataType(DataType.EmailAddress)]
+        public string Semail{ get; set; }
+        public Gender Sgender { get; set; }
+    }
+        public enum Gender
+        {
+            Male,
+            Female
+        }
+
+
+    }
